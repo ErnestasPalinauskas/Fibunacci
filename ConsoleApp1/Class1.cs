eec67fd605ea798dc089f7281d4863edd6bc0bdd
16050 <16050@C410A13-1>
2023-12-13 10:52:59 +0200

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace fib
+{
+    public class Seka
+    {
+        private double a = 2;
+        private double b = 1;
+        private double fib = 0;
+        private double limit;
+
+        public Seka(double newLimit)
+        {
+            limit = newLimit;
+        }
+
+        public void Skaiciavimas()
+        {
+            while (fib <= limit)
+            {
+                fib = a * b;
+                Console.WriteLine(fib); // Išvedimas fib rezultatas
+                a = b;
+                b = fib;
+            }
+
+            Console.WriteLine("Paskutinė reikmė iki limito : " + a);
+            Console.WriteLine("Paskutinė reikmė po limito : " + b);
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace fib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            Seka obj = new Seka(Math.Pow(10, 15));
+            obj.Skaiciavimas(); // objekto iškvietimas
+
+            stopwatch.Stop();
+            Console.WriteLine("Laikas kuris truko: " + stopwatch.Elapsed); // Veikimo laikas
+
+            Console.ReadLine();
+        }
+    }
+}
